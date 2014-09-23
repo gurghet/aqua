ebd7b9be185576d7693ff69780b5891f371645b8
Andrea <gurghet+c9@gmail.com>
2014-09-23 11:16:12 +0000

    restore neptune

--- sites/all/themes/neptune/js/script.js
+++ sites/all/themes/neptune/js/script.js
@@ -12,6 +12,81 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
 
+// Draw triangles across the top of the page.
+// Triangles are drawn to the <canvas> element (see: html.tpl.php).
+Drupal.behaviors.tri = {
+  attach: function (context, settings) {
+    $('#tri-canvas:not(.tri-processed)', context).addClass('tri-processed').each(function() {
+
+      // Retrieve window width.
+      var ww = $(window).width();
+
+      // draw().
+      function draw() {
+
+        // Retrieve canvas.
+        var canvas = document.getElementById('tri-canvas');
+
+        // If rendering contexts are available...
+        if (canvas.getContext){
+
+          // Get a 2D canvas rendering context.
+          var ctx = canvas.getContext('2d');  
+
+          // Set width equal to window width.
+          ctx.canvas.width = ww;
+
+          // Define a color offset value.
+          // This will be used when defining the triangle's style.
+          var color = 0;
+
+          // Start drawing triangles from the top left hand corner of
+          // the page and keep moving across until we run out of space.
+          for (leftCorner = 0; leftCorner < ww; leftCorner += 50) {
+
+            // Generate a random(ish) width value for the triangle.
+            // Note: the extra 20 is added to avoid anything too narrow.
+            var width = Math.floor(Math.random() * 80) + 20;
+
+            // Ensure the width value is even.
+            width = (width%2) ? width-1 : width;
+
+            // Generate a random(ish) height value for the triangle.
+            // Note: the extra 20 is added to avoid anything too short.
+            var height = Math.floor(Math.random() * 40) + 20;
+
+            // Define the triangle's style (color and opacity).
+            var style = 'rgba('+(50+color)+', '+(255-color)+', 255, 0.3)';
+
+            // Increment the color offset value.
+            color += 5;
+
+            // Draw the triangle and fill it.
+            ctx.fillStyle = style;
+            ctx.beginPath();
+            ctx.moveTo(leftCorner, 0);
+            ctx.lineTo((leftCorner+(width/2)), height);
+            ctx.lineTo(leftCorner+width, 0);
+            ctx.lineTo(leftCorner, 0);
+            ctx.fill();
+            ctx.closePath();
+          }
+        }
+      }
+
+      // Draw the triangles.
+      draw();
+
+      // Redraw the triangles on window resize.
+      $(window).resize(function() {
+        ww = $(window).width();
+        draw();
+      });
+
+    });
+  }
+};
+
 // Minify the navigation at mobile resolutions.
 Drupal.behaviors.nav = {
   attach: function (context, settings) {
